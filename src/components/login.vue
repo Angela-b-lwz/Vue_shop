3f1b4d72a05002eafb7e2d439e09809c7fe14af4
Angela-b-lwz <746259921@qq.com>
2019-12-19 16:20:13 +0800

    空格bug问题

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="login_container">
       <div class="login_box">
+        <!-- 头像区域 -->
         <div class="avatar_box">
           <img src="../assets/logo.png" alt="">
         </div>
+        <!-- 登录表单区域 -->
+          <el-form ref="loginFormRef" class="login_form" :model="loginForm" :rules="loginFormRules">
+            <el-form-item prop="username">
+              <el-input  prefix-icon="iconfont icon-users" v-model="loginForm.username"></el-input>
+            </el-form-item>
+            <el-form-item prop="password">
+              <el-input type="password" prefix-icon="iconfont icon-3702mima" v-model="loginForm.password"></el-input>
+            </el-form-item>
+            <el-form-item class="btns">
+              <el-button type="primary" @click="login">登录</el-button>
+              <el-button type="info" @click="resetLogin">重置</el-button>
+            </el-form-item>
+          </el-form>
       </div>
   </div>
 </template>
@@ -11,10 +25,23 @@
 <script>
 export default {
   data () {
-    return { }
+    return {
+      loginForm: { username: 'admin', password: '123456' },
+      loginFormRules: { username: [ { required: true, message: '请输入用户名', trigger: 'blur' }, { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' } ],
+      password: [ { required: true, message: '请输入密码', trigger: 'blur' }, { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' } ] } }
   },
-  created () { },
-  methods: { }
+  created () {
+  },
+  methods: {
+    // 重置功能调用表单resetFields()方法对整个表单进行重置，将所有字段值重置为初始值并移除校验结果
+    resetLogin() { this.$refs.loginFormRef.resetFields() },
+    login() {
+      this.$refs.loginFormRef.validate(valid => {
+         if (!valid) return
+        this.$http.post('login', this.loginForm )          
+     })
+    }
+   }
 }
 </script>
 
@@ -50,5 +77,16 @@ export default {
       background-color: #eee;
     }
   }
+}
+.login_form {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+.btns {
+  display: flex;
+  justify-content: flex-end;
 }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
 import { Button } from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+import { Form } from 'element-ui'
+import { formItem } from 'element-ui'
+import { Input } from 'element-ui'
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(formItem)
+Vue.use(Input)
