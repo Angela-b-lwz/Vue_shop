b0c279c5d9c96e2fd2c04c054cb930aa5e82fc4f
Angela-b-lwz <746259921@qq.com>
2019-12-19 16:43:56 +0800

    完成登录功能

--- src/components/login.vue
+++ src/components/login.vue
@@ -36,10 +36,18 @@ export default {
     // 重置功能调用表单resetFields()方法对整个表单进行重置，将所有字段值重置为初始值并移除校验结果
     resetLogin() { this.$refs.loginFormRef.resetFields() },
     login() {
-      this.$refs.loginFormRef.validate(valid => {
-         if (!valid) return
-        this.$http.post('login', this.loginForm )          
-     })
+      this.$refs.loginFormRef.validate(async valid => {
+        if (!valid) return
+        const { data: res } = await this.$http.post('login', this.loginForm)
+        if (res.meta.status !== 200) return this.$message.error('登录失败！')
+        this.$message.success('登录成功')
+        // 1. 将登录成功之后的 token，保存到客户端的 sessionStorage 中
+        //   1.1 项目中出了登录之外的其他API接口，必须在登录之后才能访问
+        //   1.2 token 只应在当前网站打开期间生效，所以将 token 保存在 sessionStorage 中
+        window.sessionStorage.setItem('token', res.data.token)
+        // 2. 通过编程式导航跳转到后台主页，路由地址是 /home
+   this.$router.push('/home')
+      })
     }
    }
 }
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -3,7 +3,10 @@ import { Button } from 'element-ui'
 import { Form } from 'element-ui'
 import { formItem } from 'element-ui'
 import { Input } from 'element-ui'
+import { Message } from 'element-ui'
+
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(formItem)
 Vue.use(Input)
+Vue.prototype.$message = Message
